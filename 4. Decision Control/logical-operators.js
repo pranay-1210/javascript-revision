dc4be35725dc9fb228b5e473e64f75a6243989db
Pranay <praveenpranay07@gmail.com>
2026-02-05 22:27:50 +0530

    Learnt about the different logical operators.

---
+++ 4. Decision Control/logical-operators.js
@@ -0,0 +1,15 @@
+console.log('AND Operator');
+console.log(true && true); // true
+console.log(true && false); // false
+console.log(false && true); // false
+console.log(false && false); // false
+
+console.log('OR Operator');
+console.log(true || true); // true
+console.log(true || false); // true
+console.log(false || true); // true
+console.log(false || false); // false
+
+console.log('NOT Operator');
+console.log(!true); // false
+console.log(!false); // true

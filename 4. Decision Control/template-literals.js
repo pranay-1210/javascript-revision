4a86cdab20bade246401ad7e890a1de682c3c35b
Pranay <praveenpranay07@gmail.com>
2026-02-06 10:12:45 +0530

    Learnt about the different template literals used in javascript

---
+++ 4. Decision Control/template-literals.js
@@ -0,0 +1,22 @@
+let str = "Hello, World!";
+console.log(str);
+
+let str1 = 'How are you?';
+console.log(str1);
+
+let str2 = `Dear Sir,
+
+I am giving my resignation.
+
+Thank You.
+`;
+console.log(str2);
+
+let bossName = 'John'; //This is called as String Interpolation i.e, we can embed any expressions withing a string using ${expressions}
+let str3 = `Dear ${bossName}, 
+
+I am giving my resignation.
+
+Thank You.
+`;
+console.log(str3);
